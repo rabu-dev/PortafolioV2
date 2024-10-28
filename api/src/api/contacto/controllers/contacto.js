db79bf55b0716fcce817327ce665051746850f7e
Rabu Developers <raulraposo24082001@gmail.com>
2024-10-29 00:18:05 +0100

    Eliminar carpeta nombre-de-la-carpeta

--- api/src/api/contacto/controllers/contacto.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * contacto controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::contacto.contacto');
